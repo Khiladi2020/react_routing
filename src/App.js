6732c19b8bd12f8921e4331a140fd0cf9429c81c
Ravinder Singh <ravindermaan0123@gmail.com>
2021-12-25 15:19:07 +0530

    added basic react router setup with two pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import {BrowserRouter,Route} from 'react-router-dom'
+
+import Welcome from './pages/Welcome'
+import Products from './pages/Products';
+
 function App() {
     return (
         <div className="App">
-            <h1>Hi there</h1>
+            <BrowserRouter>
+                <Route path="/welcome">
+                    <Welcome />
+                </Route>
+                <Route path="/products">
+                    <Products />
+                </Route>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = ()=>{
+    return <h2>Welcome to: Welcome page</h2>
+}
+
+export default Welcome
