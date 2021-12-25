4dd89a60f5dad981d800160c2e6cff081b3596f1
Ravinder Singh <ravindermaan0123@gmail.com>
2021-12-26 00:35:31 +0530

    added links of different dummy products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
+
 const Products = ()=>{
-    return <h2>Welcome to: Products page</h2>
+    return <section>
+        <h2>Products:</h2>
+        <ul>
+            <li><Link to="/products/book" >Book</Link></li>
+            <li><Link to="/products/car" >Car</Link></li>
+            <li><Link to="/products/course" >Course</Link></li>
+        </ul>
+    </section>
 }
 
 export default Products
